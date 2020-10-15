0f244f49114f51691e3a4987c534e1667515075a
Daniel <danielrachi1@gmail.com>
2020-10-15 15:56:07 -0500

    Agregado el metodo SixPackGenerator.filter() ahora con comentarios

--- app/src/main/java/com/example/tienbeerv20/Logic/SixPackGenerator.java
+++ app/src/main/java/com/example/tienbeerv20/Logic/SixPackGenerator.java
@@ -3,6 +3,7 @@ package com.example.tienbeerv20.Logic;
 import com.example.tienbeerv20.Data.Cerveza;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SixPackGenerator {
     private ArrayList<Cerveza> seleccion;
@@ -26,8 +27,38 @@ public class SixPackGenerator {
         this.seleccionClon = seleccionClon;
     }
 
-
-    public void Repetidas(){}
-
-    public void NoRepetidas(){}
+    //A este metodo le entra el segundo filtro y la preferencia del usuario para este filtro
+    //Borra todas las cervezas que no satisfacen la preferencia del usuario
+    public void filter(String filtro, String preferencia){
+        Iterator<Cerveza> itr = seleccionClon.iterator();
+        if (filtro.equals("nacionalidad")){
+            while (itr.hasNext()){
+                String nacionalidadNext = (String)itr.next().getNacionalidad();
+                if(!nacionalidadNext.equals(preferencia)){
+                    itr.remove();
+                }
+            }
+        }if (filtro.equals("precio")){
+            while (itr.hasNext()){
+                String precioNext = (String)itr.next().getPrecio();
+                if(!precioNext.equals(preferencia)){
+                    itr.remove();
+                }
+            }
+        }if (filtro.equals("tipo")){
+            while (itr.hasNext()){
+                String tipoNext = (String)itr.next().getTipo();
+                if(!tipoNext.equals(preferencia)){
+                    itr.remove();
+                }
+            }
+        }if (filtro.equals("alcohol")){
+            while (itr.hasNext()){
+                String alcoholNext = (String)itr.next().getAlcohol();
+                if(!alcoholNext.equals(preferencia)){
+                    itr.remove();
+                }
+            }
+        }
+    }
 }
