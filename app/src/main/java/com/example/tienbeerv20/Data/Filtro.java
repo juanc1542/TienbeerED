b778a256e94b942cd573a7738aedbf074e2800fb
Wulics <jandres.daza11@gmail.com>
2020-10-14 22:16:01 -0500

    Más clases

---
+++ app/src/main/java/com/example/tienbeerv20/Data/Filtro.java
@@ -0,0 +1,30 @@
+package com.example.tienbeerv20.Data;
+
+public class Filtro {
+
+    private String prefNacionalidad;
+    private String prefTipo;
+    private String prefAlcohol;
+    private String prefPrecio;
+    private String[] prioridad;
+
+    public String getPrefNacionalidad() {
+        return prefNacionalidad;
+    }
+
+    public String getPrefTipo() {
+        return prefTipo;
+    }
+
+    public String getPrefAlcohol() {
+        return prefAlcohol;
+    }
+
+    public String getPrefPrecio() {
+        return prefPrecio;
+    }
+
+    public String[] getPrioridad() {
+        return prioridad;
+    }
+}
