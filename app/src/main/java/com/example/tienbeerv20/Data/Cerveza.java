6ee31b941e035aef028ae6be6d200e55f8d4762d
juanc1542 <juancamilo32.17@gmail.com>
2020-10-15 15:04:22 -0500

    Adicion de atributos y metodos a cerveza

--- app/src/main/java/com/example/tienbeerv20/Data/Cerveza.java
+++ app/src/main/java/com/example/tienbeerv20/Data/Cerveza.java
@@ -1,4 +1,71 @@
 package com.example.tienbeerv20.Data;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class Cerveza {
+
+    private String nombre;
+    private String nacionalidad;
+    private String precio;
+    private String tipo;
+    private String alcohol;
+
+    //Metodo constructor
+    public Cerveza(String Nombre,String Nacionalidad,String Precio, String Tipo, String Alcohol){
+        this.nombre= Nombre;
+        this.nacionalidad= Nacionalidad;
+        this.precio= Precio;
+        this.tipo= Tipo;
+        this.alcohol= Alcohol;
+    }
+
+    //Metodo para subir el arreglo de informacion a la base de datos
+    public void subirCervezas(DatabaseReference mDatabase1){
+        String id = mDatabase1.child("Cervezas").push().getKey();
+        mDatabase1.child("Cervezas").child(id).setValue(this);
+    }
+
+
+
+    //Getters de los atributos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getAlcohol() {
+        return alcohol;
+    }
+
+    //Setters de los atributos
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setAlcohol(String alcohol) {
+        this.alcohol = alcohol;
+    }
 }
--- app/src/main/java/com/example/tienbeerv20/ui/anadir/AnadirFragment.java
+++ app/src/main/java/com/example/tienbeerv20/ui/anadir/AnadirFragment.java
@@ -12,17 +12,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tienbeerv20.Data.Cerveza;
 import com.example.tienbeerv20.R;
+import com.google.firebase.database.DatabaseReference;
 //import com.example.tienbeerv20.ui.R;
 
 public class AnadirFragment extends Fragment {
 
     private AnadirViewModel mViewModel;
+    DatabaseReference mDatabase;
 
     public static AnadirFragment newInstance() {
         return new AnadirFragment();
     }
 
+    //Metodos implementados por defecto en la creacion del Fragment
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -36,5 +40,31 @@ public class AnadirFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(AnadirViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    //Metodo de recogida de recogida y subida de informacion a la DB
+    /*
+    public void  recogerDatos() {
+        //Recoleccion de informacion en el xml del fragment
+
+        String name = TextNombre.getText().toString().trim();
+        String nacionalidad = TextDescripcion.getText().toString().trim();
+        String precio = TextNum.getText().toString().trim();
+        String tipo=spiPrecio.getSelectedItem().toString();
+        String alcohol=spiServ.getSelectedItem().toString();
+
+        //Setting all the information collected
+        Cerveza newBeer = new Cerveza(name,nacionalidad,precio,tipo,alcohol);
+        newBeer.setNombre(name);
+        newBeer.setNacionalidad(nacionalidad);
+        newBeer.setPrecio(precio);
+        newBeer.setTipo(tipo);
+        newBeer.setAlcohol(alcohol);
+
+        newBeer.subirCervezas(mDatabase);
+    }
+
+*/
+
+
 
 }
