68d4bd959553e8a05e6480d67db1674dc08cc592
JuanMoralesR <jumoralesr@unal.edu.co>
2020-10-15 15:03:42 -0500

    Clases

--- app/src/main/java/com/example/tienbeerv20/CheckOut/ShoppingCart.java
+++ app/src/main/java/com/example/tienbeerv20/CheckOut/ShoppingCart.java
@@ -1,10 +1,14 @@
 package com.example.tienbeerv20.CheckOut;
 
+import com.example.tienbeerv20.Data.SixPack;
+
 public class ShoppingCart {
 
     private long id;
     private int precioTotal;
-    //variable array sixpack
+    SixPack total[] = new SixPack[6];
+    int NumberOfSixPack = 0;
+
 
     public ShoppingCart(long id, int precioTotal){
         this.id=id;
@@ -20,4 +24,26 @@ public class ShoppingCart {
     public int getPrecioTotal() {
         return precioTotal;
     }
+//------------------------------------------------------------------------------------------------------
+
+    public  void  addSixPackToCart(SixPack New_SixPack){
+        total[NumberOfSixPack] = New_SixPack;
+        NumberOfSixPack++;
+    }
+//------------------------------------------------------------------------------------------------------
+    public void removeSixPackFromCart(int SixPackRemove){
+        int delete = SixPackRemove;
+
+        total[delete] = null;
+
+        for (int i = delete; i<total.length;i++){
+            total[i] = total[i+1];
+        }
+
+    }
+//------------------------------------------------------------------------------------------------------
+    public int CheckOut(){
+    return 0;
+    }
+//------------------------------------------------------------------------------------------------------
 }
