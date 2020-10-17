1db2fb4713fb72e8444259123751182a12185f05
Wulics <jandres.daza11@gmail.com>
2020-10-16 19:49:10 -0500

    Simplificar filtros

--- app/src/main/java/com/example/tienbeerv20/Data/Filtro.java
+++ app/src/main/java/com/example/tienbeerv20/Data/Filtro.java
@@ -5,17 +5,10 @@ import java.lang.reflect.Array;
 public class Filtro {
 
     private boolean repetidas;
-    private String[][] filtros = new String[4][2];
+    private String[][] filtros;
 
-    public Filtro(String nacionalidad, String tipo, String alcohol, String precio, int[] prioridad, boolean repetidas) {
-        filtros[prioridad[0]][1]= nacionalidad;
-        filtros[prioridad[0]][0]= "nacionalidad";
-        filtros[prioridad[1]][1]= tipo;
-        filtros[prioridad[1]][0]= "tipo";
-        filtros[prioridad[2]][1]= alcohol;
-        filtros[prioridad[2]][0]= "alcohol";
-        filtros[prioridad[3]][1]= precio;
-        filtros[prioridad[3]][0]= "precio";
+    public Filtro(String[][] filtros, boolean repetidas) {
+        this.filtros=filtros;
         this.repetidas=repetidas;
     }
 
