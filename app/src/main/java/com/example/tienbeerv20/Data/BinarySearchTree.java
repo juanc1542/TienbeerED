e0b166da96a1ec807506077d19cfa1b81eb62ea5
Danielrachi <danielrachi1@gmail.com>
2020-11-11 16:00:49 -0500

    Creado el metodo search() en la clase BST y se pulió todo el codigo de la clase BST

--- app/src/main/java/com/example/tienbeerv20/Data/BinarySearchTree.java
+++ app/src/main/java/com/example/tienbeerv20/Data/BinarySearchTree.java
@@ -5,37 +5,65 @@ import java.util.Iterator;
 
 public class BinarySearchTree {
 
-    class Node
-    {
-        Cerveza key;
-        Node left, right;
+    static class Node {
+        // Atributos
+        private Cerveza key;
+        private Node left, right;
 
+        // Constructor
         public Node(Cerveza item)
         {
             key = item;
             left = right = null;
         }
+
+        // getters and setters
+        public Cerveza getKey() {
+            return key;
+        }
+        public void setKey(Cerveza key) {
+            this.key = key;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+        public void setRight(Node right) {
+            this.right = right;
+        }
     }
 
     // Root of BST
-    Node root;
+    private Node root;
 
     // Constructor
-    BinarySearchTree()
-    {
+    BinarySearchTree() {
         root = null;
     }
 
+    // get and set root
+    public Node getRoot() {
+        return root;
+    }
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
     // This method mainly calls insertRec()
-    void insert(Cerveza key)
-    {
+    public void insert(Cerveza key) {
         root = insertRec(root, key);
     }
 
     /* A recursive function to
       insert a new key in BST */
-    Node insertRec(Node root, Cerveza key)
-    {
+    private Node insertRec(Node root, Cerveza key) {
         /* If the tree is empty,
            return a new node */
         if (root == null)
@@ -55,10 +83,29 @@ public class BinarySearchTree {
         return root;
     }
 
+
+    public Node search(Node root, Cerveza key) {
+        // Base Cases: root is null or key is present at root
+        // if is null (key is not found) return null
+        // if key is present at root, return the OBJECT beer
+        if (root==null || root.key==key)
+            return root;
+
+        // Comparator
+        int comparator = root.key.getNombre().compareTo(key.getNombre());
+
+        // Key is greater than root's key
+        if (comparator < 0)
+            return search(root.right, key);
+
+        // Key is smaller than root's key
+        return search(root.left, key);
+    }
+
 //    Node llenar(ArrayList<Cerveza> arrayList){
 //        for (Cerveza cerveza : arrayList) {
-//            this.insert(cerveza);
+//            insert(cerveza);
 //        }
-//        return this.root;
+//        return root;
 //    }
 }
