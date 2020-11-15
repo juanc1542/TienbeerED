ee493624d9f0510ded80e25c4ac7ecc983941980
juanc1542 <juancamilo32.17@gmail.com>
2020-11-15 16:18:48 -0500

    Proceso de correccion para la ventana que muestra los resultados finales de el algoritmo potente de busqueda, o de filtros

--- app/src/main/java/com/example/tienbeerv20/ui/Recycler/AdaptadorUno.java
+++ app/src/main/java/com/example/tienbeerv20/ui/Recycler/AdaptadorUno.java
@@ -1,20 +1,28 @@
 package com.example.tienbeerv20.ui.Recycler;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tienbeerv20.Data.Cerveza;
 import com.example.tienbeerv20.R;
+import com.example.tienbeerv20.ui.SeleccionCervezas.SeleccionCervezas;
+import com.example.tienbeerv20.ui.login.Logueo;
 
 import java.util.ArrayList;
 
 public class AdaptadorUno extends RecyclerView.Adapter<AdaptadorUno.ViewHolderCervezas> {
     ArrayList<Cerveza> listaCerveza;
+    ArrayList<Cerveza> seleccionados = new ArrayList<Cerveza>();
     private View.OnClickListener listener;
 
     public AdaptadorUno(ArrayList<Cerveza> listaCerveza) {
@@ -30,10 +38,34 @@ public class AdaptadorUno extends RecyclerView.Adapter<AdaptadorUno.ViewHolderCe
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdaptadorUno.ViewHolderCervezas holder, int position) {
-        holder.calif.setText(listaCerveza.get(position).getNombre());
-        holder.review.setText(listaCerveza.get(position).getNacionalidad());
+    public void onBindViewHolder(@NonNull AdaptadorUno.ViewHolderCervezas holder, final int position) {
+        holder.nombre.setText(listaCerveza.get(position).getNombre());
+        holder.nacionalidad.setText(listaCerveza.get(position).getNacionalidad());
+        holder.tipo.setText(listaCerveza.get(position).getTipo());
+        holder.alcohol.setText(listaCerveza.get(position).getAlcohol());
+        holder.precio.setText("$"+listaCerveza.get(position).getPrecio());
+
+
+        //Guardado de las cervezas seleccionadas por el usuario
+        holder.btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (seleccionados.size() <= 5){
+                    seleccionados.add(listaCerveza.get(position));
+                Toast.makeText(v.getContext(), "Elegiste "+listaCerveza.get(position).getNombre()+", llevas "+listaCerveza.size(), Toast.LENGTH_SHORT).show();
+            } else if(seleccionados.size()==6){
+                    SeleccionCervezas fragment = new SeleccionCervezas();
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("key", seleccionados);
+                    fragment.setArguments(bundle);
+                    Navigation.findNavController(v).navigate(R.id.action_busqueda_to_seleccionCervezas);
+                }
+            }
+        });
+
     }
+
+
 
 
 
@@ -47,14 +79,21 @@ public class AdaptadorUno extends RecyclerView.Adapter<AdaptadorUno.ViewHolderCe
     }
 
     public class ViewHolderCervezas extends RecyclerView.ViewHolder {
-        TextView calif, review;
+        TextView nombre, nacionalidad,tipo,alcohol, precio;
+        ImageButton btnAdicionar;
 
         public ViewHolderCervezas(@NonNull View itemView) {
             super(itemView);
+            btnAdicionar=(ImageButton) itemView.findViewById(R.id.imgAdicionar);
+
+            nombre=(TextView) itemView.findViewById(R.id.nametxt);
+            nacionalidad=(TextView) itemView.findViewById(R.id.nacionalidadtxt);
+            tipo=(TextView) itemView.findViewById(R.id.tipotxt);
+            alcohol=(TextView) itemView.findViewById(R.id.alcoholtxt);
+            precio=(TextView) itemView.findViewById(R.id.preciotxt);
 
 
-            calif=(TextView) itemView.findViewById(R.id.califtxt);
-            review=(TextView) itemView.findViewById(R.id.rewtxt);
+
         }
     }
 }
